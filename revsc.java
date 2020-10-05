a62ed302c87d61743b1ebf6026c0c9abac6823ab
Yasharth Tripathi <yasharthtripathi@gmail.com>
2020-10-05 22:42:26 +0530

    2 java programs added

---
+++ revsc.java
@@ -0,0 +1,12 @@
+/*     Write a Java Program to reverse a string without using String inbuilt function reverse().        */
+
+
+public class FinalReverseWithoutUsingInbuiltFunction {
+    public static void main(String[] args) {
+        String str = "Saket Saurav";
+        char chars[] = str.toCharArray();  // converted to character array and printed in reverse order
+        for(int i= chars.length-1; i&gt;=0; i--) {
+            System.out.print(chars[i]);
+        }
+    }
+}
